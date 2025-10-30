cd8f3c232fca097fa624ca52a119b40141da3f49
vinicius <vi.95@hotmail.com>
2025-10-30 01:21:09 -0300

    Refactor MotoControllerTest to use SpringBootTest and AutoConfigureMockMvc, removing security auto-configuration exclusions

--- src/test/java/br/com/fiap/mottucontrol/controller/MotoControllerTest.java
+++ src/test/java/br/com/fiap/mottucontrol/controller/MotoControllerTest.java
@@ -7,37 +7,29 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-
 import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import br.com.fiap.mottucontrol.controller.MotoController;
 import br.com.fiap.mottucontrol.model.Moto;
 import br.com.fiap.mottucontrol.repository.MotoRepository;
 
-@WebMvcTest(
-    value = MotoController.class, 
-    excludeAutoConfiguration = {
-        SecurityAutoConfiguration.class,
-        SecurityFilterAutoConfiguration.class 
-    }
-)
+@SpringBootTest 
+@AutoConfigureMockMvc 
 public class MotoControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockBean 
     private MotoRepository motoRepository;
 
     @Autowired
@@ -45,26 +37,21 @@ public class MotoControllerTest {
 
     @Test
     public void deveListarMotosComSucesso() throws Exception {
-        // Mock do comportamento do repositório
         when(motoRepository.findAll()).thenReturn(Collections.emptyList());
 
-        // Execução da requisição
         mockMvc.perform(get("/api/motos"))
                .andExpect(status().isOk());
     }
 
     @Test
     public void deveCriarMotoComSucesso() throws Exception {
-        // Criação de uma moto de exemplo
         Moto moto = new Moto();
         moto.setModelo("Honda Biz");
         moto.setPlaca("TEST123");
         moto.setAno(2023);
 
-        // Mock do comportamento de salvar
         when(motoRepository.save(any(Moto.class))).thenReturn(moto);
 
-        // Execução da requisição POST
         mockMvc.perform(post("/api/motos")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(moto)))
