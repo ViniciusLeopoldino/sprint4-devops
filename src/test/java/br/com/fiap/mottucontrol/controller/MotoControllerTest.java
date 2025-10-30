35917ff5d97b5d762ef7829cfeca8056edd79192
vinicius <vi.95@hotmail.com>
2025-10-30 01:10:45 -0300

    Fix import statement for SecurityAutoConfiguration in MotoControllerTest

--- src/test/java/br/com/fiap/mottucontrol/controller/MotoControllerTest.java
+++ src/test/java/br/com/fiap/mottucontrol/controller/MotoControllerTest.java
@@ -16,7 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -26,7 +26,7 @@ import br.com.fiap.mottucontrol.repository.MotoRepository;
 
 @WebMvcTest(value = MotoController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class})
 public class MotoControllerTest {
-    
+
     @Autowired
     private MockMvc mockMvc;
 
