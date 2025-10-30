2e8a3f90acf734a6b3c44811a693e386da609247
Vinicius Leopoldino <135030968+ViniciusLeopoldino@users.noreply.github.com>
2025-10-30 17:36:05 -0300

    Update MottuControlApplication.java

--- src/main/java/br/com/fiap/mottucontrol/MottuControlApplication.java
+++ src/main/java/br/com/fiap/mottucontrol/MottuControlApplication.java
@@ -2,14 +2,19 @@ package br.com.fiap.mottucontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * Classe principal da aplicação Spring Boot.
+ * * A anotação @SpringBootApplication já inclui:
+ * 1. @Configuration: Marca a classe como fonte de configuração.
+ * 2. @EnableAutoConfiguration: Tenta configurar automaticamente o Spring.
+ * 3. @ComponentScan: Varre este pacote (br.com.fiap.mottucontrol) e 
+ * todos os seus sub-pacotes (como .controller, .repository, .model)
+ * em busca de componentes.
+ * * As anotações @EnableJpaRepositories, @EntityScan e @ComponentScan explícitas
+ * foram removidas por serem redundantes e causarem conflitos com os testes "fatiados" (@WebMvcTest).
+ */
 @SpringBootApplication
-@EnableJpaRepositories("br.com.fiap.mottucontrol.repository")
-@EntityScan("br.com.fiap.mottucontrol.model")
-@ComponentScan("br.com.fiap.mottucontrol.controller")
 public class MottuControlApplication {
 
     public static void main(String[] args) {
